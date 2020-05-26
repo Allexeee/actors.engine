2333d20491d22e94b23255571c0f4b58c6880090
Pixeye <19838200+PixeyeHQ@users.noreply.github.com>
2020-05-25 18:35:21 -0700

    update

--- src/actors/actors_platform.nim
+++ src/actors/actors_platform.nim
@@ -1,5 +1,5 @@
 when defined(opengl):
-  import platform/actors_platform_standalone
+  import platforms/actors_platform_standalone
   import graphics/actors_renderer_opengl
   export actors_platform_standalone
   export actors_renderer_opengl
---
+++ src/actors/platform/target/actors_windows.nim
@@ -0,0 +1,1 @@
+import ../../actors_utils
