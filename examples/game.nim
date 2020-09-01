3173ca549ecacf8e2ae70b106c45c31a6d49ab69
Pixeye <19838200+PixeyeHQ@users.noreply.github.com>
2020-09-01 05:41:03 +0200

    Update game.nim

--- examples/game.nim
+++ examples/game.nim
@@ -14,8 +14,20 @@ var uiDebugGame : UIDebugGame
 var uis* = newSeq[UI]()
 
 proc init() =
-  shader_basic = app.shader("basic")
   uiDebugGame  = newUIDebug(uis)
+  shader_basic = app.shader("basic")
+  var aspect_ratio = app.calculate_aspect_ratio()
+
+
+#   var aspect_ratio = app.calculate_aspect_ratio()
+#   var samplers = [0'u32,1'u32]
+#   mx_ortho.ortho(-view_size * aspect_ratio, view_size * aspect_ratio, -view_size, view_size ,-100, 1000)
+#   shader1 = app.shader("basic")
+#   shader1.use()
+#   shader1.setMatrix("mx_projection", mx_ortho)
+#   shader2 = app.shader("sprite")
+#   shader2.use()
+
 
 proc update() =
   if input.down Key.Esc:
